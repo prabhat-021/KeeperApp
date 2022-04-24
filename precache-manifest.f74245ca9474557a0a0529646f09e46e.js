98b2f30d32f797242229e306be80e6aa9a1404f2
prabhat_021 <prabhatsahrawat010203@gmail.com>
2022-04-24 22:14:20 +0530

    Updates

---
+++ precache-manifest.f74245ca9474557a0a0529646f09e46e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0783251f3672e7ae233110d9d6e2eafb",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "91dcddfb80aa5ef12447",
+    "url": "/KeeperApp/static/js/2.32b9d141.chunk.js"
+  },
+  {
+    "revision": "03207ee750adb07240ee",
+    "url": "/KeeperApp/static/js/main.9ea96dd5.chunk.js"
+  },
+  {
+    "revision": "199724c1793563f9cd76",
+    "url": "/KeeperApp/static/js/runtime-main.5ba27538.js"
+  }
+]);
